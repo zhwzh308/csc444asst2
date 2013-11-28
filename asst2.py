5bdebf91c39e3850d2052aadca7982f4f33581ed
Wenzhong Zhang <Wenzhong.zhang@yahoo.com>
2013-11-28 03:31:13 -0500

    Let's not use SQL and complicate things, new design

--- asst2.py
+++ asst2.py
@@ -1,5 +1,8 @@
 # This is template assignment 2
-import sys, os, getopt
+import sys, os, getopt, io, errno
+import shutil
+# for shell utility
+import time
 # sys supports the commandline arguments.
 import sqlite3
 # sqlite3 is used to establish a database for query
@@ -8,20 +11,100 @@ class Usage(Exception):
     def __init__(self, msg):
         self.msg = msg
 
-def databaseVerifier(filename):
+def fileExist(filename):
     try:
         with open(filename):
             return True
     except IOError:
         return False
 
-# get current directory path
+def addFile(filename):
+    srcfolder = os.getcwd()
+    srcfile = srcfolder+'/'+filename
+    destfolder = srcfolder + '/.scm'
+    result = shutil.copy(srcfile, destfolder)
+
+def folderExist(foldername):
+    cwd = os.getcwd()
+    path = cwd+'/'+foldername
+    if os.path.exists(path):
+        return os.path.isdir(path)
+    else:
+        return False
+
+def scmInit(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+def fileUnderControl(filename):
+    if fileExist(filename) and folderExist('.scm'):
+        destfolder = os.getcwd()+'/.scm'
+        if fileExist(destfolder+'/'+filename):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def helpUserMakeDecision():
+    print "CSC444 Software Engineering I: Assignment 2 Source Control Management System"
+    print "python asst2.py - print this message"
+    print "python asst2.py status - return if any file is under SCM"
+    print "python asst2.py status [filename] - find out if [filename] is under SCM"
+    print "python asst2.py add [filename] - Source control [filename]"
+    print "python asst2.py commit - Commit the file whichever is under SCM"
+    print "python asst2.py diff - Diff file which is under SCM"
+
+def processArgs(argc, argv):
+    # current working dir.
+    scmdir = '.scm'
+    if argc == 1:
+        helpUserMakeDecision()
+    elif argc == 2:
+        if argv[0] == 'status':
+            if (folderExist(scmdir)):
+                print "SCM folder exist."
+            else:
+                print "No file in this directory is under SCM."
+        elif argv[0] == 'add':
+            print "Add: need filename"
+            helpUserMakeDecision()
+        elif (argv[0] == 'init'):
+            if folderExist(scmdir):
+                print "nothing to be done - this directory has file under SCM."
+            else:
+                scmInit(scmdir)
+                print "Initiate Source Control..."
+        elif argv[0] == 'help':
+            helpUserMakeDecision()
+        else:
+            print argv[0], "- command not reconized. Type help for help."
+    elif argc >= 3:
+        if argv[0] == 'status':
+            pass
+        elif argv[0] == 'add':
+            print 'adding:'
+            for item in argv[1:]:
+                if fileUnderControl(item):
+                    print ' -',item
+                else:
+                    addFile(item)
+                    print ' -',item,'added.'
+            print 'done.'
+        else:
+            print argv[0], 'is not supported.'
+    else:
+        print 'not supported'
+    return 0
+
 def main(argv=None):
     if argv is None:
         argv = sys.argv
-        # etc. replacing sys.argv with argv  in the getopt call.
-    cwd = os.getcwd()
-    print len(argv), "arguments supplied."
     try:
         try:
             opts, args = getopt.getopt(argv[1:],"h",["help"])
@@ -31,64 +114,9 @@ def main(argv=None):
         print >> sys.stderr, err.msg
         print >> sys.stderr, "for help use --help"
         return 2
-    # Error checking complete
-    if (len(argv) == 1):
-        print "Running this script with no argument is not supported!"
-        print "In order to work with this SCM, you need to use command such as"
-        print " - cwd : current working dir"
-        print " - info : about this program"
-        print " - status : system status of this directory"
-        print " - list : list all files under this directory(non-recursively)"
-    elif (len(argv)==2):
-        if (argv[1] == "cwd"):
-            print "your are executing", os.path.abspath(argv[0]), "in", cwd
-        elif (argv[1] == "info"):
-            print "This is an SCM script for CSC444 assignment 2"
-            print "The solution is provided by Ximeng Wang and Wenzhong Zhang"
-        elif (argv[1] == "status"):
-            if (databaseVerifier('myDB.db')):
-                print "The directory is under SCM."
-            else:
-                print 'The directory is not under SCM. Use add command to start'
-        elif (argv[1] == "list"):
-            # get an array of filenames in files.
-            files = os.listdir(cwd)
-            print "The directory contains the following files:"
-            for file in files:
-                print " -",file
-        elif (argv[1] == "add" or argv[1] == "del"):
-            print "Add/del: need more arguments"
-        else:
-            print argv[1], "(command not reconized)"
-            # Command type 2: argc = 3
-            # this.py
-    elif (len(argv) == 3):
-        print "3 or more arg's: Currently under development."
-        if (argv[1] == "add"):
-            if(databaseVerifier('myDB.db')):
-                print "Adding file",argv[2],"to the SCM database"
-            else:
-                print "Creating database."
-                if (databaseVerifier('myDB.db')):
-                    print "Opearation succeeded. Adding file into the database."
-                else:
-                    print 'Database creation failed. Check if you have right to the directory', cwd
-        elif (argv[1] == "del"):
-            print "delete file",argv[2],"from database"
-        else:
-            print argv[1],"is not supported."
-    else: # We know there are more args. keep print them.
-        if (argv[1]=='add'):
-            print "adding "
-        elif (argv[2]=='del'):
-            print "deleting "
-        else:
-            print "not supported"
+    return processArgs(len(argv), args)
 
-# End of main
 if __name__ == "__main__":
     sys.exit(main())
 
-def myDB(object):
-    pass
 
